f090c972f4e30557b416b5308340f5620f1f4a16
sviatoslav <bugatis15@gmail.com>
2019-09-18 14:03:58 +0300

    fix db user seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,6 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
@@ -30,7 +29,6 @@ $factory->defineAs(User::class, 'admin', function (Faker $faker) {
     return [
         'name' => 'Admin',
         'email' => 'admin@admin.com',
-        'email_verified_at' => now(),
         'password' => bcrypt('123456'),
         'remember_token' => Str::random(10),
     ];
